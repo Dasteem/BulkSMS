e6a13ea2692b5c7025096125d14fe8764894d4f1
shadykeshk <shadywallas@gmail.com>
2016-10-17 01:01:09 +0200

    Update test.php

--- test.php
+++ test.php
@@ -3,7 +3,7 @@
 require_once("BulkSMS.php");
 
 
-$obj = new BulkSMS('E7GEZLY', 'BSMS','http://smsbulk.eg.mobizone.mobi/BSMS/BSendAPI?');
+$obj = new BulkSMS('user', 'pass','http://smsbulk.eg.mobizone.mobi/BSMS/BSendAPI?');
 
 //message body , language , recipient , sender
 $obj->sendSMS('لقد تم الجز', 'ar' , 201281264677 ,'E7gezly');
