106ec30677f172f67dbfde983d756457f15fa6c1
shadykeshk <shadywallas@gmail.com>
2016-10-17 04:24:17 +0200

    Update BulkSMS.php

--- BulkSMS.php
+++ BulkSMS.php
@@ -1,88 +1,95 @@
 <?php
 
-	/**
-	 * @description BulkSMS Service To send SMS Message to Mobile Using 
-	 *
-	 * @author Karim Mostafa <Karim.Mostafa@e7gezly.com>
-	 * 
-	 * @copyright (c) 2015, E7gezly
-	 */
-	require_once('Request.php');
+/**
+ * @description BulkSMS Service To send SMS Message to Mobile Using
+ *
+ * @author Karim Mostafa <Karim.Mostafa@e7gezly.com>
+ *
+ * @copyright (c) 2015, E7gezly
+ */
+require_once('Request.php');
 
-class BulkSMS  {
+class BulkSMS
+{
 
-	/**
-	 * store BaseURL API to Call
-	 *
-	 * @var string
-	 */
-	private $base_url ;
+    /**
+     * store BaseURL API to Call
+     *
+     * @var string
+     */
+    private $base_url;
 
-	/**
-	 * store username of API to get Auth
-	 *
-	 * @var string
-	 */
-	private $username ;
-	
-	/**
-	 * store Password of API to get Auth
-	 *
-	 * @var string 
-	 */
-	private $password ;
+    /**
+     * store username of API to get Auth
+     *
+     * @var string
+     */
+    private $username;
 
-
-	/**
-	 * object from class Request 
-	 * 
-	 * @var Object RequestClass
-	 */
-	 private $request ;
+    /**
+     * store Password of API to get Auth
+     *
+     * @var string
+     */
+    private $password;
 
 
-	/**
+    /**
+     * object from class Request
+     *
+     * @var Object RequestClass
+     */
+    private $request;
+
+
+    /**
      * @description return base url when take obj from class Bsms
      * @author Karim Mostafa <karim.Mostafa@e7gezly.com>
-     * 
-	 * @param string $url
+     *
+     * @param string $url
      * @param string $userName
      * @param string $password
-     * @return 
+     * @return
      * @access public
      *
-	*/
-	public function __construct( $userName , $pwd , $url){ 
-		
-      	$this->request = new Request($url);
-      	$this->base_url = $url ;
-      	$this->username = $userName ;
-      	$this->password = $pwd ;
-	}
+     */
+    public function __construct($userName, $pwd, $url)
+    {
+
+        $this->request = new Request($url);
+        $this->base_url = $url;
+        $this->username = $userName;
+        $this->password = $pwd;
+    }
 
 
-	/**	 
-     * @description return message url 
+    /**
+     * @description return message url
      * @author Karim Mostafa <karim.Mostafa@e7gezly.com>
-     * 
-	 * @param string $message
-     * @param string $language
-     * @param integer $recipient like 201200000000
-     * @param string $sender like E7gezly
-     * @return array  
-     * @access public 
      *
-	*/
-     public function sendSMS($sms_body , $lang , $mobile , $sender_name){
-		$params = array( 
-						'user_name' => $this->username ,
-					 	'password' => $this->password ,
-					 	'msg' => $sms_body ,'lang'=>$lang , 
-					 	'recipient' => $mobile, 
-					 	'sender' => $sender_name
-					 	);
- 		$this->request->SendRequest($params);
-	}
-} 
+     * @param $sms_body
+     * @param $lang
+     * @param $mobile
+     * @param $sender_name
+     * @return array
+     * @internal param string $message
+     * @internal param string $language
+     * @internal param int $recipient like 201200000000
+     * @internal param string $sender like E7gezly
+     * @access public
+     */
+    public function sendSMS($sms_body, $lang, $mobile, $sender_name)
+    {
+        $params = array(
+            'user_name' => $this->username,
+            'password' => $this->password,
+            'msg' => $sms_body,
+            'lang' => $lang,
+            'recipient' => $mobile,
+            'sender' => $sender_name
+        );
+       return $this->request->SendRequest($params);
+    }
+}
 
 ?>
